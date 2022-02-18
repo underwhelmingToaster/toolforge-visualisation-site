4651c363cb503c47e4a439391804f48ed5f04741
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2022-02-18 08:26:46 +0100

    tooltip implementation

--- src/components/DistributionGraph.vue
+++ src/components/DistributionGraph.vue
@@ -50,6 +50,118 @@ export default {
           return item[2]
         })
       ]
+    },
+    tooltipsPlugin (opts) {
+      function init (u, opts, data) {
+        const over = u.over
+
+        const ttc = u.cursortt = document.createElement('div')
+        ttc.className = 'tooltip'
+        ttc.textContent = '(x,y)'
+        ttc.style.pointerEvents = 'none'
+        ttc.style.position = 'absolute'
+        ttc.style.background = 'rgba(0,0,255,0.1)'
+        over.appendChild(ttc)
+
+        u.seriestt = opts.series.map((s, i) => {
+          if (i === 0) return
+
+          const tt = document.createElement('div')
+          tt.className = 'tooltip'
+          tt.textContent = 'Tooltip!'
+          tt.style.pointerEvents = 'none'
+          tt.style.position = 'absolute'
+          tt.style.background = 'rgba(0,0,0,0.1)'
+          tt.style.color = s.color
+          tt.style.display = s.show ? null : 'none'
+          over.appendChild(tt)
+          return tt
+        })
+
+        function hideTips () {
+          ttc.style.display = 'none'
+          u.seriestt.forEach((tt, i) => {
+            if (i === 0) return
+
+            tt.style.display = 'none'
+          })
+        }
+
+        function showTips () {
+          ttc.style.display = null
+          u.seriestt.forEach((tt, i) => {
+            if (i === 0) return
+
+            const s = u.series[i]
+            tt.style.display = s.show ? null : 'none'
+          })
+        }
+
+        over.addEventListener('mouseleave', () => {
+          if (!u.cursor.lock) {
+            // u.setCursor({left: -10, top: -10});
+            hideTips()
+          }
+        })
+
+        over.addEventListener('mouseenter', () => {
+          showTips()
+        })
+
+        hideTips()
+      }
+
+      function setCursor (u) {
+        const { left, top, idx } = u.cursor
+
+        // this is here to handle if initial cursor position is set
+        // not great (can be optimized by doing more enter/leave state transition tracking)
+        // if (left > 0)
+        // u.cursortt.style.display = null;
+
+        u.cursortt.style.left = left + 'px'
+        u.cursortt.style.top = top + 'px'
+        u.cursortt.textContent = '(' + u.posToVal(left, 'x').toFixed(2) + ', ' + u.posToVal(top, 'y').toFixed(2) + ')'
+
+        // can optimize further by not applying styles if idx did not change
+        u.seriestt.forEach((tt, i) => {
+          if (i === 0) return
+
+          const s = u.series[i]
+
+          if (s.show) {
+            // this is here to handle if initial cursor position is set
+            // not great (can be optimized by doing more enter/leave state transition tracking)
+            // if (left > 0)
+            // tt.style.display = null;
+
+            const xVal = u.data[0][idx]
+            const yVal = u.data[i][idx]
+
+            tt.textContent = '(' + xVal + ', ' + yVal + ')'
+
+            tt.style.left = Math.round(u.valToPos(xVal, 'x')) + 'px'
+            tt.style.top = Math.round(u.valToPos(yVal, s.scale)) + 'px'
+          }
+        })
+      }
+
+      return {
+        hooks: {
+          init,
+          setCursor,
+          setScale: [
+            (u, key) => {
+              console.log('setScale', key)
+            }
+          ],
+          setSeries: [
+            (u, idx) => {
+              console.log('setSeries', idx)
+            }
+          ]
+        }
+      }
     }
   },
   beforeMount () {
@@ -101,6 +213,9 @@ export default {
             stroke: 'blue',
             fill: 'blue'
           }
+        ],
+        plugins: [
+          this.tooltipsPlugin()
         ]
       }
     }
