3e36483eea6e47a5c908c3357af4abea3506dbe3
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2022-02-28 10:33:37 +0100

    custom marker

--- src/components/JsMap.vue
+++ src/components/JsMap.vue
@@ -5,6 +5,7 @@
 <script>
 import 'leaflet/dist/leaflet.css'
 import * as L from 'leaflet/dist/leaflet'
+import * as markerSVG from '@/assets/marker.svg'
 let map
 let marker
 
@@ -19,13 +20,18 @@ export default {
     }
   },
   mounted () {
+    const icon = L.icon({
+      iconUrl: markerSVG,
+      iconSize: [50, 50]
+    })
+
     map = L.map('map').setView([51.505, -0.09], 2)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
 
-    marker = L.marker(this.markerLoc).addTo(map)
+    marker = L.marker(this.markerLoc, { icon: icon }).addTo(map)
   }
 }
 </script>
